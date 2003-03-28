fc17ec95507803bd941f227cebf9f202fbff4db1
Richard Levitte <richard@levitte.org>
2003-03-28 16:57:36 +0000

    Start working on alpha5
    Monotone-Parent: f1d84bec9072ae117ca09722bc6fbe6483e99d14
    Monotone-Revision: fe092dd937fbdaf7b77cba1595bb52b7f97e7665

--- version.c
+++ version.c
@@ -52,8 +52,8 @@
 
 #ifdef I18N
 char *Version = "MIT X Consortium, R6, ctwm 3.7 + I18N Patch ";
-char *VersionNumber = "3.7-alpha4";
+char *VersionNumber = "3.7-alpha5";
 #else
 char *Version = "MIT X Consortium, R6, ctwm 3.7";
-char *VersionNumber = "3.7-alpha4";
+char *VersionNumber = "3.7-alpha5";
 #endif
