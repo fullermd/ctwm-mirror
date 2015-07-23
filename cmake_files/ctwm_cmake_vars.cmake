45a69482004b3d1a0e1a80741137b2421964b407
Matthew Fuller <fullermd@over-yonder.net>
2015-07-23 08:19:17 -0500

    move the CTWMSRC/CTWMLIBS initializations into our vars file.

--- cmake_files/ctwm_cmake_vars.cmake
+++ cmake_files/ctwm_cmake_vars.cmake
@@ -4,3 +4,36 @@
 
 # The dir in which we ship pregen'd source files
 set(GENSRCDIR ${CMAKE_CURRENT_SOURCE_DIR}/gen)
+
+
+# Our base set of sources
+set(CTWMSRC
+	add_window.c
+	clargs.c
+	clicktofocus.c
+	ctwm.c
+	ctwm_atoms.c
+	cursor.c
+	events.c
+	gc.c
+	gram.tab.c
+	iconmgr.c
+	icons.c
+	lex.c
+	list.c
+	menus.c
+	mwmhints.c
+	otp.c
+	parse.c
+	resize.c
+	session.c
+	util.c
+	version.c
+	vscreen.c
+	windowbox.c
+	workmgr.c
+)
+
+
+# Libs to link in (init empty list)
+set(CTWMLIBS)
