a027bdae9879aced39818b1c6fc379ac3dc20d89
Matthew Fuller <fullermd@over-yonder.net>
2015-07-24 17:17:04 -0500

    Put COMMENT's on these build cases so it's clearer what's going on.

--- cmake_files/gen_source_files.cmake
+++ cmake_files/gen_source_files.cmake
@@ -36,6 +36,7 @@ if(IS_BZR_CO AND HAS_BZR)
 	add_custom_command(OUTPUT ${version_c}
 		DEPENDS ${version_c_in} ${BZR_DIRSTATE_FILE}
 		COMMAND ${CMAKE_SOURCE_DIR}/tools/rewrite_version_bzr.sh < ${version_c_in} > ${version_c}
+		COMMENT "Generating version.c from current WT state."
 	)
 else()
 	# Is there a prebuilt one to use?
@@ -44,12 +45,14 @@ else()
 		add_custom_command(OUTPUT ${version_c}
 			DEPENDS ${GENSRCDIR}/version.c
 			COMMAND cp ${GENSRCDIR}/version.c ${version_c}
+			COMMENT "Using pregenerated version.c."
 		)
 	else()
 		# Nope
 		add_custom_command(OUTPUT ${version_c}
 			DEPENDS ${version_c_in}
 			COMMAND sed -e 's/%%REVISION%%/NULL/' < ${version_c_in} > ${version_c}
+			COMMENT "Using null version.c."
 		)
 	endif(EXISTS ${GENSRCDIR}/version.c)
 endif(IS_BZR_CO AND HAS_BZR)
