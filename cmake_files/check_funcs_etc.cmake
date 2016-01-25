54e7e80f5b1dded9b36d4765b72e20045a835802
Matthew Fuller <fullermd@over-yonder.net>
2016-01-24 19:10:50 -0600

    Add a check in cmake for asprintf() so we can warn the user.  Leave a note in the comment about a source for a portable version if we run into a case where we need to pull in a local implementation, but I'm not expecting to.

--- cmake_files/check_funcs_etc.cmake
+++ cmake_files/check_funcs_etc.cmake
@@ -27,3 +27,15 @@ endif(NOT HAS_GETOPT_H)
 if(NOT HAS_GETOPT_LONG)
 	message(FATAL_ERROR "You don't seem to have getopt_long().")
 endif(NOT HAS_GETOPT_LONG)
+
+
+
+# asprintf(3) has been in glibc since at least the early 90's, the BSD's
+# since the mid 90's, and [Open]Solaris since 11.  It's not in Sol10, at
+# least some versions of AIX, etc.  There's a version in openssh-portable
+# that I believe is pretty portable if we find a system we care about
+# lacking it and need to pull in a local version, but I don't expect to.
+check_function_exists(asprintf HAS_ASPRINTF)
+if(NOT HAS_ASPRINTF)
+	message(FATAL_ERROR "You don't seem to have asprintf(3).")
+endif(NOT HAS_ASPRINTF)
