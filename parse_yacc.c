f2aebc75622e3becf9b728b383893900c685e267
Matthew Fuller <fullermd@over-yonder.net>
2016-01-31 22:48:37 -0600

    Pull the static code out of gram.y and into a seaprate parse_yacc.c. Right now this is a small amount, but I hope to pull more of the inline stuff out into fixed funcs over time.

---
+++ parse_yacc.c
@@ -0,0 +1,252 @@
+/*
+ * Parser routines called from yacc code (gram.y)
+ */
+
+#include "ctwm.h"
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <strings.h>
+
+#include "otp.h"
+#include "menus.h"
+#include "icons.h"
+#include "windowbox.h"
+#include "add_window.h"
+#include "list.h"
+#include "util.h"
+#include "screen.h"
+#include "parse.h"
+#include "parse_be.h"
+#include "parse_yacc.h"
+#include "cursor.h"
+
+char *Action = "";
+char *Name = "";
+char *defstring = "default";
+MenuRoot *root, *pull = NULL;
+char *curWorkSpc;
+char *client, *workspace;
+MenuItem *lastmenuitem = (MenuItem*) 0;
+
+char *ptr;
+name_list **list;
+int cont = 0;
+int color;
+int mods = 0;
+unsigned int mods_used = (ShiftMask | ControlMask | Mod1Mask);
+
+
+void yyerror(char *s)
+{
+    twmrc_error_prefix();
+    fprintf (stderr, "error in input file:  %s\n", s ? s : "");
+    ParseError = 1;
+}
+
+void InitGramVariables(void)
+{
+    mods = 0;
+}
+
+void RemoveDQuote(char *str)
+{
+    char *i, *o;
+    int n;
+    int count;
+
+    for (i=str+1, o=str; *i && *i != '\"'; o++)
+    {
+	if (*i == '\\')
+	{
+	    switch (*++i)
+	    {
+	    case 'n':
+		*o = '\n';
+		i++;
+		break;
+	    case 'b':
+		*o = '\b';
+		i++;
+		break;
+	    case 'r':
+		*o = '\r';
+		i++;
+		break;
+	    case 't':
+		*o = '\t';
+		i++;
+		break;
+	    case 'f':
+		*o = '\f';
+		i++;
+		break;
+	    case '0':
+		if (*++i == 'x')
+		    goto hex;
+		else
+		    --i;
+	    case '1': case '2': case '3':
+	    case '4': case '5': case '6': case '7':
+		n = 0;
+		count = 0;
+		while (*i >= '0' && *i <= '7' && count < 3)
+		{
+		    n = (n<<3) + (*i++ - '0');
+		    count++;
+		}
+		*o = n;
+		break;
+	    hex:
+	    case 'x':
+		n = 0;
+		count = 0;
+		while (i++, count++ < 2)
+		{
+		    if (*i >= '0' && *i <= '9')
+			n = (n<<4) + (*i - '0');
+		    else if (*i >= 'a' && *i <= 'f')
+			n = (n<<4) + (*i - 'a') + 10;
+		    else if (*i >= 'A' && *i <= 'F')
+			n = (n<<4) + (*i - 'A') + 10;
+		    else
+			break;
+		}
+		*o = n;
+		break;
+	    case '\n':
+		i++;	/* punt */
+		o--;	/* to account for o++ at end of loop */
+		break;
+	    case '\"':
+	    case '\'':
+	    case '\\':
+	    default:
+		*o = *i++;
+		break;
+	    }
+	}
+	else
+	    *o = *i++;
+    }
+    *o = '\0';
+}
+
+MenuRoot *GetRoot(char *name, char *fore, char *back)
+{
+    MenuRoot *tmp;
+
+    tmp = FindMenuRoot(name);
+    if (tmp == NULL)
+	tmp = NewMenuRoot(name);
+
+    if (fore)
+    {
+	int save;
+
+	save = Scr->FirstTime;
+	Scr->FirstTime = TRUE;
+	GetColor(COLOR, &tmp->highlight.fore, fore);
+	GetColor(COLOR, &tmp->highlight.back, back);
+	Scr->FirstTime = save;
+    }
+
+    return tmp;
+}
+
+void GotButton (int butt, int func)
+{
+    int i;
+    MenuItem *item;
+
+    for (i = 0; i < NUM_CONTEXTS; i++) {
+	if ((cont & (1 << i)) == 0) continue;
+
+	if (func == F_MENU) {
+	    pull->prev = NULL;
+	    AddFuncButton (butt, i, mods, func, pull, (MenuItem*) 0);
+	}
+	else {
+	    root = GetRoot (TWM_ROOT, NULLSTR, NULLSTR);
+	    item = AddToMenu (root, "x", Action, NULL, func, NULLSTR, NULLSTR);
+	    AddFuncButton (butt, i, mods, func, (MenuRoot*) 0, item);
+	}
+    }
+
+    Action = "";
+    pull = NULL;
+    cont = 0;
+    mods_used |= mods;
+    mods = 0;
+}
+
+void GotKey(char *key, int func)
+{
+    int i;
+
+    for (i = 0; i < NUM_CONTEXTS; i++)
+    {
+	if ((cont & (1 << i)) == 0)
+	  continue;
+
+	if (func == F_MENU) {
+	    pull->prev = NULL;
+	    if (!AddFuncKey (key, i, mods, func, pull, Name, Action)) break;
+	}
+	else
+	if (!AddFuncKey(key, i, mods, func, (MenuRoot*) 0, Name, Action))
+	  break;
+    }
+
+    Action = "";
+    pull = NULL;
+    cont = 0;
+    mods_used |= mods;
+    mods = 0;
+}
+
+
+void GotTitleButton (char *bitmapname, int func, Bool rightside)
+{
+    if (!CreateTitleButton (bitmapname, func, Action, pull, rightside, True)) {
+	twmrc_error_prefix();
+	fprintf (stderr,
+		 "unable to create %s titlebutton \"%s\"\n",
+		 rightside ? "right" : "left", bitmapname);
+    }
+    Action = "";
+    pull = NULL;
+}
+
+Bool CheckWarpScreenArg (char *s)
+{
+    if (strcasecmp (s,  WARPSCREEN_NEXT) == 0 ||
+	strcasecmp (s,  WARPSCREEN_PREV) == 0 ||
+	strcasecmp (s,  WARPSCREEN_BACK) == 0)
+      return True;
+
+    for (; *s && Isascii(*s) && Isdigit(*s); s++) ; /* SUPPRESS 530 */
+    return (*s ? False : True);
+}
+
+
+Bool CheckWarpRingArg (char *s)
+{
+    if (strcasecmp (s,  WARPSCREEN_NEXT) == 0 ||
+	strcasecmp (s,  WARPSCREEN_PREV) == 0)
+      return True;
+
+    return False;
+}
+
+
+Bool CheckColormapArg (char *s)
+{
+    if (strcasecmp (s, COLORMAP_NEXT) == 0 ||
+	strcasecmp (s, COLORMAP_PREV) == 0 ||
+	strcasecmp (s, COLORMAP_DEFAULT) == 0)
+      return True;
+
+    return False;
+}
---
+++ parse_yacc.h
@@ -0,0 +1,38 @@
+/*
+ * Header for the various parse_yacc/gram.y backend funcs
+ */
+#ifndef _PARSE_YACC_H
+#define _PARSE_YACC_H
+
+void yyerror(char *s);
+
+void InitGramVariables(void);
+void RemoveDQuote(char *str);
+
+MenuRoot *GetRoot(char *name, char *fore, char *back);
+
+Bool CheckWarpScreenArg(char *s);
+Bool CheckWarpRingArg(char *s);
+Bool CheckColormapArg(char *s);
+void GotButton(int butt, int func);
+void GotKey(char *key, int func);
+void GotTitleButton(char *bitmapname, int func, Bool rightside);
+
+
+extern char *Action;
+extern char *Name;
+extern char *defstring;
+extern MenuRoot *root, *pull;
+extern char *curWorkSpc;
+extern char *client, *workspace;
+extern MenuItem *lastmenuitem;
+
+extern char *ptr;
+extern name_list **list;
+extern int cont;
+extern int color;
+extern int mods;
+extern unsigned int mods_used;
+
+
+#endif /* _PARSE_YACC_H */
