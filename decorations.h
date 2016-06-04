9dc655e37a3f401012b364a2d34d6d9727a18ca1
Matthew Fuller <fullermd@over-yonder.net>
2016-06-04 02:06:36 -0500

    Now we can remove the gray/black bits from decorations.h as they're no longer used anywhere.

--- decorations.h
+++ decorations.h
@@ -25,20 +25,4 @@ void PaintTitleButton(TwmWindow *tmp_win, TBWindow  *tbw);
 void PaintBorders(TwmWindow *tmp_win, bool focus);
 
 
-/*
- * Used in decorations.c and add_window.c for building images.
- *
- * XXX Maybe this should be moved in the image* and just have a function
- * to return the bitmaps, instead of manually building them?
- */
-#define gray_width 2
-#define gray_height 2
-static char gray_bits[] = {
-	0x02, 0x01
-};
-static char black_bits[] = {
-	0xFF, 0xFF
-};
-
-
 #endif /* _CTWM_DECORATIONS_H */
