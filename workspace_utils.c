772767e35e22ecc4bdb4a6ee43e38f06a6dbf1a7
Matthew Fuller <fullermd@over-yonder.net>
2016-08-06 21:53:23 -0500

    Move GotoWorkSpace up to alongside the other frontends that call it.

--- workspace_utils.c
+++ workspace_utils.c
@@ -226,57 +226,6 @@ GotoDownWorkSpace(VirtualScreen *vs)
 }
 
 
-
-/*
- * Show the background (by hiding all windows) or undo it.
- * f.showbackground, also can be called via EWMH bits.
- *
- * newstate is the desired showing state.
- * Pass -1 to toggle, 1 to show the background,
- * or 0 to re-show the windows.
- *
- * XXX Doesn't really belong here; more of a functions.c-ish thing
- * probably.  But left here for the moment.
- */
-void
-ShowBackground(VirtualScreen *vs, int newstate)
-{
-	static int state = 0;
-	TwmWindow *twmWin;
-
-	if(newstate == state) {
-		return;
-	}
-
-	if(state) {
-		for(twmWin = Scr->FirstWindow; twmWin != NULL; twmWin = twmWin->next) {
-			if(twmWin->savevs == vs) {
-				DisplayWin(vs, twmWin);
-			}
-			twmWin->savevs = NULL;
-		}
-		state = 0;
-	}
-	else {
-		for(twmWin = Scr->FirstWindow; twmWin != NULL; twmWin = twmWin->next) {
-			if(twmWin->vs == vs
-#ifdef EWMH
-			                /* leave wt_Desktop and wt_Dock visible */
-			                && twmWin->ewmhWindowType == wt_Normal
-#endif /* EWMH */
-			  ) {
-				twmWin->savevs = twmWin->vs;
-				Vanish(vs, twmWin);
-			}
-		}
-		state = 1;
-	}
-#ifdef EWMH
-	EwmhSet_NET_SHOWING_DESKTOP(state);
-#endif /* EWMH */
-}
-
-
 /*
  * Belongs with the above GotoWorkSpace* funcs
  */
@@ -536,6 +485,57 @@ GotoWorkSpace(VirtualScreen *vs, WorkSpace *ws)
 		set_last_window(newws);
 	}
 	MaybeAnimate = true;
+}
+
+
+
+/*
+ * Show the background (by hiding all windows) or undo it.
+ * f.showbackground, also can be called via EWMH bits.
+ *
+ * newstate is the desired showing state.
+ * Pass -1 to toggle, 1 to show the background,
+ * or 0 to re-show the windows.
+ *
+ * XXX Doesn't really belong here; more of a functions.c-ish thing
+ * probably.  But left here for the moment.
+ */
+void
+ShowBackground(VirtualScreen *vs, int newstate)
+{
+	static int state = 0;
+	TwmWindow *twmWin;
+
+	if(newstate == state) {
+		return;
+	}
+
+	if(state) {
+		for(twmWin = Scr->FirstWindow; twmWin != NULL; twmWin = twmWin->next) {
+			if(twmWin->savevs == vs) {
+				DisplayWin(vs, twmWin);
+			}
+			twmWin->savevs = NULL;
+		}
+		state = 0;
+	}
+	else {
+		for(twmWin = Scr->FirstWindow; twmWin != NULL; twmWin = twmWin->next) {
+			if(twmWin->vs == vs
+#ifdef EWMH
+			                /* leave wt_Desktop and wt_Dock visible */
+			                && twmWin->ewmhWindowType == wt_Normal
+#endif /* EWMH */
+			  ) {
+				twmWin->savevs = twmWin->vs;
+				Vanish(vs, twmWin);
+			}
+		}
+		state = 1;
+	}
+#ifdef EWMH
+	EwmhSet_NET_SHOWING_DESKTOP(state);
+#endif /* EWMH */
 }
 
 
