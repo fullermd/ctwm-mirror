67a9d384a49a3d0153c499b89f5b307d8d1452e1
Matthew Fuller <fullermd@over-yonder.net>
2016-08-15 09:48:41 -0500

    Comment out the glibc workaround, and the BSD workaround for what that workaround causes.  Add commented out Sun Studio version of -std=c99 in place of just mentioning it in a comment, for easier enabling. Tweak some comments.

--- minibuild/defs.mk
+++ minibuild/defs.mk
@@ -5,22 +5,22 @@ YACC_FLAGS?=-d -b gram
 YACC_CMD?=${YACC} ${YACC_FLAGS}
 FLEX?=flex
 
-# Alt: -xc99 for Sun Studio
-C99FLAG?=-std=c99
+C99FLAG?=-std=c99   # Most gcc-like compilers
+#C99FLAG=-xc99      # Sun Studio
 
 # Basic flags for build and pulling in X
 _CFLAGS=${C99FLAG} -O -I${RTDIR} -I${RTDIR}/ext -I${BDIR} -I/usr/local/include
 _LFLAGS=-L/usr/local/lib -lSM -lICE -lX11 -lXext -lXmu -lXt
 
 # glibc headers desire these when in C99 mode
-_CFLAGS+=-D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
+#_CFLAGS+=-D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
 
-# And BSD headers, when they see the above, need this
-_CFLAGS+=-D__BSD_VISIBLE
+# And BSD headers, if you enable the above, need this
+#_CFLAGS+=-D__BSD_VISIBLE
 
 # Solaris b0rkedness needs this for gethostbyname
 #_LFLAGS+=-lnsl
-# and for asprintf declaration
+# and for asprintf declaration, if you set the glibc flags
 #_CFLAGS+=-D__EXTENSIONS__
 
 
