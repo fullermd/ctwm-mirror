dbc23769f59c69fc1fefce0dafb8d4d2f6d996dc
Matthew Fuller <fullermd@over-yonder.net>
2016-10-14 02:38:45 -0500

    Add the gen script to the list of dependancies, to make life easier for those of us working on it.

--- cmake_files/gen_source_files.cmake
+++ cmake_files/gen_source_files.cmake
@@ -38,12 +38,13 @@ list(APPEND CTWMSRC ${CMAKE_CURRENT_BINARY_DIR}/event_names_table.h)
 
 # Create function bits
 set(fd_list ${CMAKE_CURRENT_SOURCE_DIR}/functions_defs.list)
+set(fd_mk   ${CMAKE_CURRENT_SOURCE_DIR}/tools/mk_function_bits.sh)
 set(fd_h    ${CMAKE_CURRENT_BINARY_DIR}/functions_defs.h)
 set(fdd_h   ${CMAKE_CURRENT_BINARY_DIR}/functions_deferral.h)
 add_custom_command(
 	OUTPUT ${fd_h} ${fdd_h}
-	DEPENDS ${fd_list}
-	COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/tools/mk_function_bits.sh ${fd_list} ${CMAKE_CURRENT_BINARY_DIR}
+	DEPENDS ${fd_list} ${fd_mk}
+	COMMAND ${fd_mk} ${fd_list} ${CMAKE_CURRENT_BINARY_DIR}
 )
 list(APPEND CTWMSRC ${fd_h} ${fdd_h})
 
