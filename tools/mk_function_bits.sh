c77b4307e8418596b0f5c0ec7bfc0f9dee843c90
Matthew Fuller <fullermd@over-yonder.net>
2016-10-29 13:41:02 -0500

    Update comment on this block.

--- tools/mk_function_bits.sh
+++ tools/mk_function_bits.sh
@@ -263,8 +263,9 @@ EOF
 #
 gf="${outdir}/functions_dispatch_execution.h"
 (
-	# Everything we need for this is in the main section.
-	# Now run 'em both together and output
+	# Functions in the main and synthetic sections need dispatching.
+	# Order doesn't matter in defining this, so we can just run 'em
+	# together without worrying about sorting.
 	print_header
 	cat << EOF
 #ifndef _CTWM_FUNCTIONS_DISPATCH_EXECUTION_H
