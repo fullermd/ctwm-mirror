e8ca6255cd708ab30dae0473bbb91962325aa76d
Matthew Fuller <fullermd@over-yonder.net>
2017-01-25 05:04:07 -0600

    Add a little to the a comment about some seemingly-redundant code. Should probably check these assumptions a bit.

--- add_window.c
+++ add_window.c
@@ -690,7 +690,13 @@ AddWindow(Window w, AWType wtype, IconMgr *iconp, VirtualScreen *vs)
 	winbox = findWindowBox(tmp_win);
 
 
-	/* Start figuring frame sizes */
+	/*
+	 * Set some plausible values for the frame size.
+	 *
+	 * XXX These get redone down below when we create the frame.  So why
+	 * bother here?  I think there's some code in the middle that needs
+	 * at least a reasonable guess onhand...
+	 */
 	tmp_win->frame_width  = tmp_win->attr.width  + 2 * tmp_win->frame_bw3D;
 	tmp_win->frame_height = tmp_win->attr.height + 2 * tmp_win->frame_bw3D +
 	                        tmp_win->title_height;
