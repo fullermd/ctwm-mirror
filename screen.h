fdb853566162f89e83f3e2a9c6438749ed4b16d9
Matthew Fuller <fullermd@over-yonder.net>
2017-11-18 10:00:08 -0600

    Move some of these color bits together into a single block instead of having them spread out.

--- screen.h
+++ screen.h
@@ -384,6 +384,49 @@ struct ScreenInfo {
 	/// Color of window borders.  \sa ScreenInfo.BorderColorL
 	ColorPair BorderColorC;
 
+	/// Specialized border colors for windows.  From BorderColor config
+	/// var.  \sa ScreenInfo.BorderColorC
+	name_list *BorderColorL;
+
+	/// Specialized border colors for icons.  From IconBorderColor config
+	/// var.  \sa ScreenInfo.IconBorderColor
+	name_list *IconBorderColorL;
+
+	/// Specialized border coloring.  From BorderTileForeground config
+	/// var.  \sa ScreenInfo.BorderTileC
+	name_list *BorderTileForegroundL;
+
+	/// \copydoc ScreenInfo::BorderTileForegroundL
+	name_list *BorderTileBackgroundL;
+
+	/// Specialized titlebar foreground coloring.  From TitleForeground
+	/// config var.  \sa ScreenInfo.TitleC
+	name_list *TitleForegroundL;
+
+	/// Specialized titlebar background coloring.  From TitleBackground
+	/// config var.  \sa ScreenInfo.TitleC
+	name_list *TitleBackgroundL;
+
+	/// Specialized icon foreground coloring.  From IconForeground
+	/// config var.  \sa ScreenInfo.IconC
+	name_list *IconForegroundL;
+
+	/// Specialized icon background coloring.  From IconBackground
+	/// config var.  \sa ScreenInfo.IconC
+	name_list *IconBackgroundL;
+
+	/// Specialized icon manager foreground coloring.  From
+	/// IconManagerForeground config var.  \sa ScreenInfo.IconManagerC
+	name_list *IconManagerFL;
+
+	/// Specialized icon manager background coloring.  From
+	/// IconManagerBackground config var.  \sa ScreenInfo.IconManagerC
+	name_list *IconManagerBL;
+
+	/// Color to highlight focused windows in icon manager.
+	/// \sa ScreenInfo.IconManagerHighlight
+	name_list *IconManagerHighlightL;
+
 	/// Menu shadow color
 	Pixel MenuShadowColor;
 
@@ -509,55 +552,6 @@ struct ScreenInfo {
 	struct OtpPreferences *OTP;
 	/// Stash of OTP info about icons on the screen. \copydetails OTP
 	struct OtpPreferences *IconOTP;
-
-	/**
-	 * \addtogroup scr_color_bits Various color definitions.
-	 * @{
-	 */
-	/// Specialized border colors for windows.  From BorderColor config
-	/// var.  \sa ScreenInfo.BorderColorC
-	name_list *BorderColorL;
-
-	/// Specialized border colors for icons.  From IconBorderColor config
-	/// var.  \sa ScreenInfo.IconBorderColor
-	name_list *IconBorderColorL;
-
-	/// Specialized border coloring.  From BorderTileForeground config
-	/// var.  \sa ScreenInfo.BorderTileC
-	name_list *BorderTileForegroundL;
-
-	/// \copydoc ScreenInfo::BorderTileForegroundL
-	name_list *BorderTileBackgroundL;
-
-	/// Specialized titlebar foreground coloring.  From TitleForeground
-	/// config var.  \sa ScreenInfo.TitleC
-	name_list *TitleForegroundL;
-
-	/// Specialized titlebar background coloring.  From TitleBackground
-	/// config var.  \sa ScreenInfo.TitleC
-	name_list *TitleBackgroundL;
-
-	/// Specialized icon foreground coloring.  From IconForeground
-	/// config var.  \sa ScreenInfo.IconC
-	name_list *IconForegroundL;
-
-	/// Specialized icon background coloring.  From IconBackground
-	/// config var.  \sa ScreenInfo.IconC
-	name_list *IconBackgroundL;
-
-	/// Specialized icon manager foreground coloring.  From
-	/// IconManagerForeground config var.  \sa ScreenInfo.IconManagerC
-	name_list *IconManagerFL;
-
-	/// Specialized icon manager background coloring.  From
-	/// IconManagerBackground config var.  \sa ScreenInfo.IconManagerC
-	name_list *IconManagerBL;
-
-	/// Color to highlight focused windows in icon manager.
-	/// \sa ScreenInfo.IconManagerHighlight
-	name_list *IconManagerHighlightL;
-	/// @}
-
 
 	/// From IconManagers config var.  This is a mapping from the window
 	/// name pattern to the IconMgr structure it should go in.  All the
