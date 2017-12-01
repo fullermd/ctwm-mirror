d097daf38bf0a9dad4a5c59e460666f875eb97cd
Matthew Fuller <fullermd@over-yonder.net>
2017-11-30 22:56:36 -0600

    Since we moved the find-yacc code up earlier, there's now no reason to do this whole thing in a giant else() rather than just using elseif() to separate the 3 cases better, so do that.

--- cmake_files/setup_yacc.cmake
+++ cmake_files/setup_yacc.cmake
@@ -34,43 +34,39 @@ if(BISON_FOUND)
 	string(REPLACE ";" " " _YFSTR "${YFLAGS}")
 	BISON_TARGET(ctwm_parser gram.y ${CMAKE_CURRENT_BINARY_DIR}/gram.tab.c
 		COMPILE_FLAGS ${_YFSTR})
+elseif(YACC)
+	# Got yacc(1), use it
+	message(STATUS "Found yacc: ${YACC}")
+	add_custom_command(OUTPUT gram.tab.c gram.tab.h
+		DEPENDS gram.y
+		COMMAND ${YACC} ${YFLAGS} ${CMAKE_CURRENT_SOURCE_DIR}/gram.y
+		COMMENT "Building parser with yacc."
+	)
 else()
-	# There doesn't seem to be a standard module for yacc, so hand-code
-	# it.
-	if(YACC)
-		# Got yacc(1), use it
-		message(STATUS "Found yacc: ${YACC}")
-		add_custom_command(OUTPUT gram.tab.c gram.tab.h
-			DEPENDS gram.y
-			COMMAND ${YACC} ${YFLAGS} ${CMAKE_CURRENT_SOURCE_DIR}/gram.y
-			COMMENT "Building parser with yacc."
+	# No bison, no yacc.  Maybe there are prebuilt files?
+	find_file(GRAM_C gram.tab.c
+		PATHS ${GENSRCDIR} NO_DEFAULT_PATH)
+	find_file(GRAM_H gram.tab.h
+		PATHS ${GENSRCDIR} NO_DEFAULT_PATH)
+	if(GRAM_C AND GRAM_H)
+		# Got prebuilt ones, use 'em
+		message(STATUS "No yacc found, using prebuilt gram.tab.*")
+		add_custom_command(OUTPUT gram.tab.h
+			DEPENDS ${GRAM_H}
+			COMMAND cp ${GRAM_H} .
+			COMMENT "Copying in prebuilt gram.tab.h."
 		)
+		add_custom_command(OUTPUT gram.tab.c
+			DEPENDS ${GRAM_C}
+			COMMAND cp ${GRAM_C} .
+			COMMENT "Copying in prebuilt gram.tab.c."
+		)
+		# Also need to explicitly tell cmake; otherwise it knows to
+		# pull in gram.tab.c ('cuz it's in CTWMSRC) but doesn't know
+		# in time to pull in gram.tab.h and so blows up.
+		set_source_files_properties(gram.tab.c OBJECT_DEPENDS gram.tab.h)
 	else()
-		# No bison, no yacc.  Maybe there are prebuilt files?
-		find_file(GRAM_C gram.tab.c
-			PATHS ${GENSRCDIR} NO_DEFAULT_PATH)
-		find_file(GRAM_H gram.tab.h
-			PATHS ${GENSRCDIR} NO_DEFAULT_PATH)
-		if(GRAM_C AND GRAM_H)
-			# Got prebuilt ones, use 'em
-			message(STATUS "No yacc found, using prebuilt gram.tab.*")
-			add_custom_command(OUTPUT gram.tab.h
-				DEPENDS ${GRAM_H}
-				COMMAND cp ${GRAM_H} .
-				COMMENT "Copying in prebuilt gram.tab.h."
-			)
-			add_custom_command(OUTPUT gram.tab.c
-				DEPENDS ${GRAM_C}
-				COMMAND cp ${GRAM_C} .
-				COMMENT "Copying in prebuilt gram.tab.c."
-			)
-			# Also need to explicitly tell cmake; otherwise it knows to
-			# pull in gram.tab.c ('cuz it's in CTWMSRC) but doesn't know
-			# in time to pull in gram.tab.h and so blows up.
-			set_source_files_properties(gram.tab.c OBJECT_DEPENDS gram.tab.h)
-		else()
-			# No bison, no yacc, no prebuilt.  Boom.
-			message(FATAL_ERROR "Can't find yacc.")
-		endif(GRAM_C AND GRAM_H)
-	endif(YACC)
+		# No bison, no yacc, no prebuilt.  Boom.
+		message(FATAL_ERROR "Can't find yacc.")
+	endif(GRAM_C AND GRAM_H)
 endif(BISON_FOUND)
