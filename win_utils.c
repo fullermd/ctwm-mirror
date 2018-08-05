cb546201eeb1d9f34f33a135db26d2f7949a610d
Matthew Fuller <fullermd@over-yonder.net>
2018-08-05 18:56:34 -0500

    Move vars to the inner scope where they're used.

--- win_utils.c
+++ win_utils.c
@@ -222,13 +222,12 @@ GetWMPropertyString(Window w, Atom prop)
 		return NULL;
 	}
 
-	char **text_list;
-	int  text_list_count;
-
 	if(text_prop.encoding == XA_STRING
 	                || text_prop.encoding == XA_UTF8_STRING
 	                || text_prop.encoding == XA_COMPOUND_TEXT) {
 		/* property is encoded as compound text - convert to locale string */
+		char **text_list;
+		int  text_list_count;
 		int status = XmbTextPropertyToTextList(dpy, &text_prop, &text_list,
 		                                       &text_list_count);
 		if(text_list_count == 0
