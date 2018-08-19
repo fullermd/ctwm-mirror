ca0ea576ba8b74803a33ec65f42dd14ed8e4e429
Matthew Fuller <fullermd@over-yonder.net>
2018-08-19 16:33:19 -0500

    Reorder lines for consistency.

--- event_handlers.c
+++ event_handlers.c
@@ -1882,8 +1882,8 @@ void HandleDestroyNotify(void)
 
 	FreeWMPropertyString(Tmp_win->names.ctwm_wm_name); // 2
 	FreeWMPropertyString(Tmp_win->names.wm_name);      // 2
-	FreeWMPropertyString(Tmp_win->names.wm_icon_name); // 3
 	FreeWMPropertyString(Tmp_win->names.ctwm_wm_icon_name); // 3
+	FreeWMPropertyString(Tmp_win->names.wm_icon_name); // 3
 #ifdef EWMH
 	FreeWMPropertyString(Tmp_win->names.net_wm_name);      // 2
 	FreeWMPropertyString(Tmp_win->names.net_wm_icon_name); // 3
