7d72a71ca82e8039a64b6c449e4f4b11da914146
Matthew Fuller <fullermd@over-yonder.net>
2020-05-03 16:08:19 -0500

    Simplify test.
    I had more twisting in mind when I made the extra layer of macros, but
    wound up not needing anything so involved, so GC the vestiges of the
    overcomplication.

--- tests/menu_twmkeys/mk_twmkeys_entry.c
+++ tests/menu_twmkeys/mk_twmkeys_entry.c
@@ -14,17 +14,13 @@ int
 main(int argc, char *argv[])
 {
 	FuncKey key;
-	int mods;
 
-#define SET() do { \
-                memset(&key, 0, sizeof(key)); \
-                key.name   = "KEY"; \
-                key.action = "ACT"; \
-                key.mods   = mods; \
-        } while(0) \
+	// Init
+	memset(&key, 0, sizeof(key));
+	key.name   = "KEY";
+	key.action = "ACT";
 
 #define TST(expect) do { \
-                SET(); \
                 const char *ret = mk_twmkeys_entry(&key); \
                 if(strcmp(ret, expect) != 0) { \
                         fprintf(stderr, "Expected '%s', got '%s'\n", \
@@ -34,31 +30,30 @@ main(int argc, char *argv[])
         } while(0)
 
 	// Simple
-	mods = ShiftMask;
+	key.mods = ShiftMask;
 	TST("[S+KEY] ACT");
 
-	mods = ControlMask;
+	key.mods = ControlMask;
 	TST("[C+KEY] ACT");
 
 	// Combo
-	mods = Mod1Mask | Alt1Mask;
+	key.mods = Mod1Mask | Alt1Mask;
 	TST("[M+A1+KEY] ACT");
 
-	mods = Alt1Mask | Alt2Mask | Alt3Mask | Alt4Mask | Alt5Mask;
+	key.mods = Alt1Mask | Alt2Mask | Alt3Mask | Alt4Mask | Alt5Mask;
 	TST("[A1+A2+A3+A4+A5+KEY] ACT");
 
 	// All the mods!
-	mods = ShiftMask | ControlMask
-	       | Mod1Mask | Mod2Mask | Mod3Mask | Mod4Mask | Mod5Mask
-	       | Alt1Mask | Alt2Mask | Alt3Mask | Alt4Mask | Alt5Mask;
+	key.mods = ShiftMask | ControlMask
+	           | Mod1Mask | Mod2Mask | Mod3Mask | Mod4Mask | Mod5Mask
+	           | Alt1Mask | Alt2Mask | Alt3Mask | Alt4Mask | Alt5Mask;
 	TST("[M+S+C+M2+M3+M4+M5+A1+A2+A3+A4+A5+KEY] ACT");
 
 
 	// Magic value used to test overflow
 	{
 		// Overflow by 1: inherit mods from above, and add a bit
-		mods |= 1 << 30;
-		SET();
+		key.mods |= 1 << 30;
 
 		const char *ret = mk_twmkeys_entry(&key);
 		if(ret != NULL) {
@@ -69,8 +64,7 @@ main(int argc, char *argv[])
 	}
 	{
 		// Overflow by itself
-		mods = 1 << 31;
-		SET();
+		key.mods = 1 << 31;
 
 		const char *ret = mk_twmkeys_entry(&key);
 		if(ret != NULL) {
