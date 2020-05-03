5c25b3a88b40894d80ff3ab8e6d361dc1d402db7
Matthew Fuller <fullermd@over-yonder.net>
2020-05-02 20:12:24 -0500

    Drop a skeleton in place to test mk_twmkeys_entry.

---
+++ tests/menu_twmkeys/mk_twmkeys_entry.c
@@ -0,0 +1,19 @@
+/*
+ * Test mk_twmkeys_entry()
+ */
+
+#include "ctwm.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "menu.h"
+
+int
+main(int argc, char *argv[])
+{
+
+	// NOTYET
+	exit(0);
+}
