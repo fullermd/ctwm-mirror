2d473e74abe6aea5b6a75f2f58a532f4f979e5d6
Matthew Fuller <fullermd@over-yonder.net>
2020-05-02 20:39:54 -0500

    Add some straightforward tests.

--- tests/menu_twmkeys/mk_twmkeys_entry.c
+++ tests/menu_twmkeys/mk_twmkeys_entry.c
@@ -8,12 +8,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "menu.h"
+#include "menus.h"
 
 int
 main(int argc, char *argv[])
 {
+	FuncKey key;
+	int mods;
 
-	// NOTYET
+#define SET() do { \
+		memset(&key, 0, sizeof(key)); \
+		key.name   = "KEY"; \
+		key.action = "ACT"; \
+		key.mods   = mods; \
+	} while(0) \
+
+#define TST(expect) do { \
+		SET(); \
+		const char *ret = mk_twmkeys_entry(&key); \
+		if(strcmp(ret, expect) != 0) { \
+			fprintf(stderr, "Expected '%s', got '%s'\n", expect, ret); \
+			exit(1); \
+		} \
+	} while(0)
+
+	// Simple
+	mods = ShiftMask;
+	TST("[S+KEY] ACT");
+
+	mods = ControlMask;
+	TST("[C+KEY] ACT");
+
+	// Combo
+	mods = Mod1Mask | Alt1Mask;
+	TST("[M+A1+KEY] ACT");
+
+	mods = Alt1Mask | Alt2Mask | Alt3Mask | Alt4Mask | Alt5Mask;
+	TST("[A1+A2+A3+A4+A5+KEY] ACT");
+
+
+	// OK then
 	exit(0);
 }
