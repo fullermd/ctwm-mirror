217174f60072f4308698026afbc7cfe3baffea5c
Maxime Soulé <btik-git@scoubidou.com>
2020-07-11 16:39:40 -0500

    Adjust the repositioning when squeezeing away a window and the titlebar is also squeezed.  The 2d border is outside the width, so it doesn't figure into that, but does figure into the new X coordinate.

--- win_ops.c
+++ win_ops.c
@@ -271,8 +271,8 @@ Squeeze(TwmWindow *tmp_win)
 		fy += tmp_win->frame_height - newh;
 	}
 	if(tmp_win->squeeze_info) {
-		fx  += tmp_win->title_x - tmp_win->frame_bw3D;
-		neww = tmp_win->title_width + 2 * (tmp_win->frame_bw + tmp_win->frame_bw3D);
+		fx  += tmp_win->title_x + tmp_win->frame_bw - tmp_win->frame_bw3D;
+		neww = tmp_win->title_width + 2 * tmp_win->frame_bw3D;
 	}
 
 	eventMask = mask_out_event(tmp_win->w, StructureNotifyMask);
