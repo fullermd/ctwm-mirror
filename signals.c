97690c7d4d0dd70124697f3435009c044c20a2e5
Olaf 'Rhialto' Seibert <rhialto@falu.nl>
2024-01-20 16:26:22 -0600

    Switch to using a SIGCHLD handler to reap children, instead of SIG_IGN'ing.  The intersection of some weird interactions between bits of POSIX and implementation decisions means that on some systems it can cause some unpleasant behavior.

--- signals.c
+++ signals.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 #include "ctwm_shutdown.h"
 #include "signals.h"
@@ -16,6 +18,7 @@
 /* Our backends */
 static void sh_restart(int signum);
 static void sh_shutdown(int signum);
+static void sh_sigchld(int signum);
 
 
 // Internal flags for which signals have called us
@@ -46,9 +49,8 @@ setup_signal_handlers(void)
 	// die...
 	signal(SIGALRM, SIG_IGN);
 
-	// This should be set by default, but just in case; explicitly don't
-	// leave zombies.
-	signal(SIGCHLD, SIG_IGN);
+	// Explicitly don't leave zombies.
+	signal(SIGCHLD, sh_sigchld);
 
 	return;
 }
@@ -123,3 +125,21 @@ sh_shutdown(int signum)
 	SignalFlag = sig_shutdown = true;
 }
 
+/**
+ * Handle SIGCHLD so we don't leave zombie child processes.
+ *
+ * SIG_IGN'ing would be the easy thing to do, but due to vagaries in
+ * POSIX some implementations might wind up behaving badly.  e.g., NetBSD
+ * as of 10.0 will hang in system(3) if SICHLD is IGN'd.
+ */
+static void
+sh_sigchld(int signum)
+{
+	pid_t pid;
+	int old_errno = errno;
+
+	while((pid = waitpid(-1, NULL, WNOHANG)) > 0)
+		;
+
+	errno = old_errno;
+}
